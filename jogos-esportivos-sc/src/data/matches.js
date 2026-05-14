90db2a2f74a545a18e7de5cd3905b14952af106b
TiagoS4 <tiagosallesmelo@hotmail.com>
2026-05-14 19:15:33 -0300

    Vários aprimoramentos

--- jogos-esportivos-sc/src/data/matches.js
+++ jogos-esportivos-sc/src/data/matches.js
@@ -1,5 +1,19 @@
+import AtleticoGO from "../assets/AtleticoGO.png";
+import Blumenau from "../assets/Blumenau.png";
+import Caravaggio from "../assets/Caravaggio.png";
+import CRB from "../assets/CRB.png";
 import Criciuma from "../assets/Criciuma.png";
-import Juventude from "../assets/Juventude.png"
+import Cuiaba from "../assets/Cuiaba.png";
+import Fluminense from "../assets/Fluminense.png";
+import GuaraniDePalhoca from "../assets/GuaraniDePalhoca.png";
+import HercilioLuz from "../assets/HercilioLuz.png";
+import Juventude from "../assets/Juventude.png";
+import Juventus from "../assets/Juventus.png";
+import Metropolitano from "../assets/Metropolitano.png";
+import NacaoEsportes from "../assets/NacaoEsportes.png";
+import OperarioFerroviario from "../assets/OperarioFerroviario.png";
+import SCJaraguaDoSul from "../assets/SCJaraguaDoSul.png";
+import Tubarao from "../assets/Tubarao.png";
 
 export const featuredMatches = [
     {
@@ -13,7 +27,7 @@ export const featuredMatches = [
         homeScore: 0,
         awayScore: 0,
         status: "Encerrado",
-        time: "19:00 - 21:00",
+        time: "21:15",
         date: "09/05/2026",
         live: false
     },
@@ -21,86 +35,267 @@ export const featuredMatches = [
 
 export const recentMatches = [
     {
+        id: 1,
+
+        championship: "Campeonato Brasileiro Série B",
+        stadium: "Arena Pantanal",
+
+        homeTeam: "Cuiabá",
+        awayTeam: "Criciúma",
+
+        homeLogo: Cuiaba,
+        awayLogo: Criciuma,
+
+        homeScore: 1,
+        awayScore: 1,
+
+        date: "02/05/2026",
+        time: "xx:xx",
+        status: "Encerrado"
+    },
+
+    {
         id: 2,
 
         championship: "Campeonato Brasileiro Série B",
-        stadium: "Alfredo Jaconi",
+        stadium: "Heriberto Hulse",
 
         homeTeam: "Criciúma",
-        awayTeam: "Juventude",
+        awayTeam: "CRB",
 
         homeLogo: Criciuma,
-        awayLogo: Juventude,
+        awayLogo: CRB,
 
-        homeScore: 2,
-        awayScore: 0,
+        homeScore: 3,
+        awayScore: 1,
 
-        date: "10/05/2026",
-        time: "19:00",
+        date: "26/04/2026",
+        time: "xx:xx",
         status: "Encerrado"
     },
 
     {
         id: 3,
 
-        championship: "Campeonato Brasileiro Série B",
-        stadium: "Alfredo Jaconi",
+        championship: "Campeonato Catarinense Série B",
+        stadium: "Aníbal Torres Costa",
 
-        homeTeam: "Criciúma",
-        awayTeam: "Juventude",
+        homeTeam: "Hercílio Luz",
+        awayTeam: "Guarani de Palhoça",
 
-        homeLogo: Criciuma,
-        awayLogo: Juventude,
+        homeLogo: HercilioLuz,
+        awayLogo: GuaraniDePalhoca,
 
-        homeScore: 2,
+        homeScore: 6,
         awayScore: 0,
 
         date: "10/05/2026",
-        time: "19:00",
+        time: "xx:xx",
         status: "Encerrado"
-    }
-];
+    },
 
-export const upcomingMatches = [
     {
         id: 4,
 
-        championship: "Campeonato Brasileiro Série B",
-        stadium: "Alfredo Jaconi",
+        championship: "Campeonato Catarinense Série B",
+        stadium: "Arena Joinville",
 
-        homeTeam: "Criciúma",
-        awayTeam: "Juventude",
+        homeTeam: "Fluminense",
+        awayTeam: "Hercílio Luz",
 
-        homeLogo: Criciuma,
-        awayLogo: Juventude,
+        homeLogo: Fluminense,
+        awayLogo: HercilioLuz,
 
-        homeScore: 2,
+        homeScore: 1,
         awayScore: 0,
 
-        date: "10/05/2026",
-        time: "19:00",
+        date: "06/05/2026",
+        time: "20:00",
         status: "Encerrado"
     },
 
     {
         id: 5,
 
-        championship: "Campeonato Brasileiro Série B",
-        stadium: "Alfredo Jaconi",
+        championship: "Campeonato Catarinense Série B",
+        stadium: "Aníbal Torres Costa",
 
-        homeTeam: "Criciúma",
-        awayTeam: "Juventude",
+        homeTeam: "Hercílio Luz",
+        awayTeam: "Blumenau",
 
-        homeLogo: Criciuma,
-        awayLogo: Juventude,
+        homeLogo: HercilioLuz,
+        awayLogo: Blumenau,
+
+        homeScore: 0,
+        awayScore: 1,
+
+        date: "13/05/2026",
+        time: "15:00",
+        status: "Encerrado"
+    },
+
+    {
+        id: 6,
+
+        championship: "Campeonato Catarinense Série B",
+        stadium: "Estádio da Montanha (SC)",
+
+        homeTeam: "Caravaggio",
+        awayTeam: "Metropolitano",
+
+        homeLogo: Caravaggio,
+        awayLogo: Metropolitano,
 
         homeScore: 2,
         awayScore: 0,
 
         date: "10/05/2026",
-        time: "19:00",
+        time: "15:00",
         status: "Encerrado"
-    }
+    },
+
+    {
+        id: 7,
+
+        championship: "Campeonato Catarinense Série B",
+        stadium: "Renato Silveira",
+
+        homeTeam: "Guarani de Palhoça",
+        awayTeam: "Caravaggio",
+
+        homeLogo: GuaraniDePalhoca,
+        awayLogo: Caravaggio,
+
+        homeScore: 1,
+        awayScore: 1,
+
+        date: "06/05/2026",
+        time: "15:00",
+        status: "Encerrado"
+    },
+
+    {
+        id: 8,
+
+        championship: "Campeonato Brasileiro Série B",
+        stadium: "Arena Joinville",
+
+        homeTeam: "Nação Esportes",
+        awayTeam: "Tubarão",
+
+        homeLogo: NacaoEsportes,
+        awayLogo: Tubarao,
+
+        homeScore: 0,
+        awayScore: 0,
+
+        date: "12/05/2026",
+        time: "19:30",
+        status: "Encerrado"
+    },
+
+    {
+        id: 9,
+
+        championship: "Campeonato Brasileiro Série B",
+        stadium: "Domingos Gonzales",
+
+        homeTeam: "Tubarão",
+        awayTeam: "Jaraguá-SC",
+
+        homeLogo: Tubarao,
+        awayLogo: SCJaraguaDoSul,
+
+        homeScore: 1,
+        awayScore: 1,
+
+        date: "06/05/2026",
+        time: "15:00",
+        status: "Encerrado"
+    },
+];
+
+export const upcomingMatches = [
+    {
+        id: 1,
+
+        championship: "Campeonato Brasileiro Série B",
+        stadium: "João Marcatto",
+
+        homeTeam: "Juventus-SC",
+        awayTeam: "Hercílio Luz",
+
+        homeLogo: Juventus,
+        awayLogo: HercilioLuz,
+
+        //homeScore: 2,
+        //awayScore: 0,
+
+        date: "26/05/2026",
+        time: "15:00",
+        status: "Marcado"
+    },
+
+    {
+        id: 2,
+
+        championship: "Campeonato Brasileiro Série B",
+        stadium: "João Marcatto",
+
+        homeTeam: "Jaraguá-SC",
+        awayTeam: "Caravaggio",
+
+        homeLogo: SCJaraguaDoSul,
+        awayLogo: Caravaggio,
+
+        //homeScore: 2,
+        //awayScore: 0,
+
+        date: "17/05/2026",
+        time: "15:00",
+        status: "Marcado"
+    },
+
+    {
+        id: 3,
+
+        championship: "Campeonato Brasileiro Série B",
+        stadium: "Domingos Gonzalez",
+
+        homeTeam: "Tubarão",
+        awayTeam: "Caravaggio",
+
+        homeLogo: Tubarao,
+        awayLogo: Caravaggio,
+
+        //homeScore: 2,
+        //awayScore: 0,
+
+        date: "24/05/2026",
+        time: "15:00",
+        status: "Marcado"
+    },
+
+    {
+        id: 4,
+
+        championship: "Campeonato Brasileiro Série B",
+        stadium: "SESI Blumenau",
+
+        homeTeam: "Metropolitano",
+        awayTeam: "Tubarão",
+
+        homeLogo: Metropolitano,
+        awayLogo: Tubarao,
+
+        //homeScore: 2,
+        //awayScore: 0,
+
+        date: "16/05/2026",
+        time: "15:00",
+        status: "Marcado"
+    },
+
 ];
 
 export const teamUpcomingMatches = {
@@ -127,6 +322,78 @@ export const teamUpcomingMatches = {
             date: "21/05",
             time: "18:00"
         }
+    ],
+
+    Hercílio_Luz: [
+        {
+            id: 4,
+            opponent: "Juventus-SC",
+            date: "24/05",
+            time: "15:00"
+        }
+
+    ],
+
+    Juventus_SC: [
+        {
+            id: 5,
+            opponent: "Hercílio Luz",
+            date: "24/05",
+            time: "15:00"
+        }
+
+    ],
+
+    Jaraguá_SC: [
+        {
+            id: 6,
+            opponent: "Caravaggio",
+            date: "17/05",
+            time: "15:00"
+        }
+
+    ],
+
+    Caravaggio: [
+        {
+            id: 7,
+            opponent: "Jaraguá-SC",
+            date: "17/05",
+            time: "15:00"
+        },
+
+        {
+            id: 8,
+            opponent: "Tubarão",
+            date: "24/05",
+            time: "15:00"
+        }
+
+    ],
+
+    Tubarão: [
+        {
+            id: 9,
+            opponent: "Caravaggio",
+            date: "24/05",
+            time: "15:00"
+        },
+
+        {
+            id: 10,
+            opponent: "Metropolitano",
+            date: "16/05",
+            time: "15:00"
+        },
+    ],
+
+    Metropolitano: [
+        {
+            id: 11,
+            opponent: "Tubarão",
+            date: "16/05",
+            time: "15:00"
+        }
     ]
 };
 
@@ -141,5 +408,41 @@ export const teams = [
         id: 2,
         name: "Juventude",
         logo: Juventude
+    },
+
+    {
+        id: 3,
+        name: "Hercílio_Luz",
+        logo: HercilioLuz
+    },
+
+    {
+        id: 4,
+        name: "Juventus_SC",
+        logo: Juventus
+    },
+
+    {
+        id: 5,
+        name: "Jaraguá_SC",
+        logo: SCJaraguaDoSul
+    },
+
+    {
+        id: 6,
+        name: "Caravaggio",
+        logo: Caravaggio
+    },
+
+    {
+        id: 7,
+        name: "Tubarão",
+        logo: Tubarao
+    },
+
+    {
+        id: 8,
+        name: "Metropolitano",
+        logo: Metropolitano
     }
 ];
